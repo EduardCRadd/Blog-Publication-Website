f0094454e6cde0d7ea2416c491bcc119713b7b49
EduardCRadd <eduard.radd@gmail.com>
2023-04-11 20:13:25 +0100

    second commit

--- scripts.js
+++ scripts.js
@@ -1,27 +1,3 @@
-// const sidebarItems = document.querySelectorAll(".sidebar__item");
-// const firstItemSvg = sidebarItems[0].querySelector("svg");
-
-// sidebarItems.forEach((item) => {
-//   const link = item.querySelector(".sidebar__link");
-//   const svg = item.querySelector("svg");
-
-//   link.addEventListener("click", () => {
-//     if (item.classList.contains("active")) {
-//       return;
-//     }
-
-//     sidebarItems.forEach((otherItem) => {
-//       otherItem.classList.remove("active");
-//       otherItem.querySelector("svg").style.display = "none";
-//     });
-
-//     item.classList.add("active");
-//     svg.style.display = "inline-block";
-//   });
-// });
-
-// firstItemSvg.style.display = "inline-block";
-
 
 const sidebarItems = document.querySelectorAll(".sidebar__item");
 const firstItemSvg = sidebarItems[0].querySelector("svg");
@@ -58,3 +34,14 @@ sidebarItems.forEach((item) => {
 });
 
 firstItemSvg.style.display = "inline-block";
+
+
+// const gridContainer = document.getElementById('blog__right-grid');
+// let currentGap = parseInt(window.getComputedStyle(gridContainer).gap);
+
+// window.addEventListener('resize', () => {
+//   const newGap = currentGap + 10; // increase gap by 10 pixels
+//   gridContainer.style.gap = `${newGap}px`;
+//   currentGap = newGap;
+// });
+
